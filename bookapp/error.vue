f356ed9634dd52320f3195f2aae25a5ee393cc36
Mika Tejada <mikayla.tejada@cambridge.org>
2023-11-29 18:15:15 +0800

    error page navigateTo

--- bookapp/error.vue
+++ bookapp/error.vue
@@ -2,12 +2,17 @@
     <div class="flex h-screen justify-center items-center flex-col">
         <h1 class="text-9xl">404</h1>
         <p class="mt-7 text-4xl">Page not found</p>
-        <button @click="handleError" class="mt-7 rounded-full bg-sky-300 hover:bg-sky-400 px-3 py-1 float-right ml-2">Go to login</button>
+        <button @click="handleError" class="mt-7 rounded-full bg-sky-300 hover:bg-sky-400 px-3 py-1 float-right ml-2">Go to home</button>
         </div>
 </template>
 
 <script setup> 
 const handleError = () => {
-    navigateTo('/')
+    const token = localStorage.getItem('token')
+    if (token==='undefined' || token === null) {
+        navigateTo('/')
+    } else {
+        navigateTo('/api/books')
+    }
 }
 </script>
